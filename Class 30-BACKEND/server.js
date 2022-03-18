54e1db05da5fb84fcad688ece6108e073bbab260
Abrar124 <abrar.khurshid.124@gmail.com>
2022-03-17 22:36:45 -0700

    modules G-object

---
+++ Class 30-BACKEND/server.js
@@ -0,0 +1,16 @@
+// setInterval(() => {
+//   console.log("Function run after 1 seconds");
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// const fs = require('fs');
+// fs.writeFileSync("text.txt", "Hello node modules")
+// fs.appendFileSync("text.txt", " append file")
+
+// const readText = fs.readFileSync("text.txt")
+// const changeText = readText.toString()
+// console.log(changeText)
+
+
